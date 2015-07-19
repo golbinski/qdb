c1ce58a58c426427f636bfab65e174c6a7aa007a
golbinski <gracjan.olbinski@gmail.com>
2015-07-19 12:11:33 +0200

    added trace test for flushing correctly with using of the print log handler

--- qdb/trace_tests.cc
+++ qdb/trace_tests.cc
@@ -8,10 +8,34 @@
 // mock print handler
 namespace qdb
 {
+namespace testing
+{
+namespace
+{
 
+std::string* gBuffer = nullptr;
+
+struct TmpBuffer
+{
+    std::string data;
+
+    TmpBuffer()
+    {
+        gBuffer = &data;
+    }
+
+    ~TmpBuffer()
+    {
+        gBuffer = nullptr;
+    }
+};  // struct TmpBuffer
+
+}  // unnamed namespace
+}  // namespace testing
 void PrintLog(const std::string& message)
 {
-    std::cout << message << std::endl;
+    ASSERT_TRUE(::qdb::testing::gBuffer != nullptr);
+    *::qdb::testing::gBuffer = message;
 }
 
 }  // namespace qdb
@@ -21,10 +45,21 @@ namespace qdb
 namespace testing
 {
 
-TEST(Random, succeed)
+struct TraceShould : ::testing::Test
 {
-    QDB_TRACE << "Hello World!?";
-    QDB_TRACE << "Next line of log! Awesome!";
+    TmpBuffer buffer;
+};  // test fixture TraceShould
+
+bool HasSubstr(const std::string& str, const std::string& substr)
+{
+    return str.find(substr) != std::string::npos;
+}
+
+TEST_F(TraceShould, FlushMessageUsingPrintHandler)
+{
+    const std::string message{"example message"};
+    QDB_TRACE << message;
+    ASSERT_PRED2(HasSubstr, buffer.data, message);
 }
 
 }  // namespace testing
